3297245aea3f669266b4c0c0ba8499e97f2ebcd3
ncabuk@ai-ag.de <ncabuk@ai-ag.de>
2025-04-22 13:41:15 +0200

    Django läuft im Container, Migration erfolgreich, Startseite erreichbar

--- config/settings.py
+++ config/settings.py
@@ -74,14 +74,20 @@ WSGI_APPLICATION = 'config.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': 'todoapp',
-        'USER': 'dein_pg_user',
-        'PASSWORD': 'dein_passwort',
-        'HOST': 'localhost',
-        'PORT': '5432',
+        #'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        #'NAME': 'todoapp',
+        #'USER': 'dein_pg_user',
+        #'PASSWORD': 'dein_passwort',
+        #'HOST': 'localhost',
+        #'PORT': '5432',
+
+
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / "db.sqlite3",
     }
 }
+
+
 
 
 # Password validation
