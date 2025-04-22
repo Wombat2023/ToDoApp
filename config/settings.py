c1c6df8bc11ae781aec93a6274edcb9704bb1c57
ncabuk@ai-ag.de <ncabuk@ai-ag.de>
2025-04-22 14:09:14 +0200

    Migrationen angewendet und Server erfolgreich gestartet

--- config/settings.py
+++ config/settings.py
@@ -37,6 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    
+    'tasks',  # Deine eigene App hinzufügen
 ]
 
 MIDDLEWARE = [
--- tasks/views.py
+++ tasks/views.py
@@ -1,3 +1,5 @@
 from django.shortcuts import render
 
+def home(request):
+    return render(request, 'tasks/home.hmtl')
 # Create your views here.
