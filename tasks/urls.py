78bf15d4a2c0acc029ca5757298abea478a418c9
ncabuk@ai-ag.de <ncabuk@ai-ag.de>
2025-04-25 20:20:20 +0200

    Bearbeiten/Löschen eingebaut: URLs + Views ergänzt, Templates angepasst

--- tasks/urls.py
+++ tasks/urls.py
@@ -1,11 +1,12 @@
 from django.contrib import admin
 from django.urls import path
 #from django.urls import admin / Wird nicht gebraucht, da Admin nicht aus django.urls kommt sondern aus contrib! 
-from tasks.views import startseite
-
+#from tasks.views import startseite / Wird nicht mehr gebraucht dafür, wird from . import views verwendet!
 from . import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', views.startseite, name='startseite'),
+    path('bearbeiten/<int:pk>/', views.aufgabe_bearbeiten, name='aufgabe_bearbeiten'),
+    path('loeschen/<int:pk>/', views.aufgabe_loeschen, name='aufgabe_loeschen')
 ]
--- tasks/views.py
+++ tasks/views.py
@@ -1,7 +1,23 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect, get_object_or_404
 from .models import Aufgabe
 
 
 def startseite(request):
     aufgaben = Aufgabe.objects.all()
     return render(request, 'tasks/startseite.html', {'aufgaben': aufgaben})
+
+def aufgabe_bearbeiten(request, pk):
+    aufgabe = get_object_or_404(Aufgabe, pk=pk)
+    if request.method == 'POST':
+        aufgabe.titel = request.POST['titel']
+        aufgabe.beschreibung = request.POST['beschreibung']
+        aufgabe.erledigt = request.POST['beschreibung']
+        aufgabe.erledigt = 'erledigt' in request.POST
+        aufgabe.save()
+        return redirect('startseite')
+    return render(request, 'tasks/bearbeiten.html', {'aufgabe': aufgabe})
+
+def aufgabe_loeschen(request, pk):
+    aufgabe = get_object_or_404(Aufgabe, pk=pk)
+    aufgabe.delete()
+    return redirect('startseite')
