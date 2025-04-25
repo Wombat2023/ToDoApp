8585f7d8262a806ff3a4aeb83e019af9f6743d82
ncabuk@ai-ag.de <ncabuk@ai-ag.de>
2025-04-25 12:49:05 +0200

    Modell Aufgabe angelegt und im Adminbereich registriert, Superuser erfolgreich erstellt

--- tasks/models.py
+++ tasks/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
-# Create your models here.
+class Aufgabe(models.Model):
+    titel = models.CharField(max_length=200)
+    beschreibung = models.TextField()
+    erledigt = models.BooleanField(default=False)
+    erstellt_am = models.DateTimeField(auto_now_add=True)
+
+
+def __str__(self):
+    return self.titel
